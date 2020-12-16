e5460d5ac1cb9e6e22222051df7ccb2d6686eaad
user184905 <user184905@FF065.local>
2020-12-16 12:35:07 +0100

    WIP

--- iBench/UI/Modules/OnboardingFlow/HowToViewController.swift
+++ iBench/UI/Modules/OnboardingFlow/HowToViewController.swift
@@ -6,8 +6,124 @@
 //  Copyright © 2020 Андрей Журавлев. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 class HowToViewController: BaseViewController {
     
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var pageControl: UIPageControl!
+    
+    var cellsIds = [
+        "Welcome Cell",
+        "id2",
+        "iThirdScreen"
+    ]
+    
+    private var currentPageIndex = 0 {
+        didSet {
+            guard oldValue != currentPageIndex else {
+                return
+            }
+            updatePageControllContainerView()
+            updateCollectionView()
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+    }
+    
+    func update() {
+        guard isViewLoaded else {
+            return
+        }
+        
+        updatePageControllContainerView()
+        updateCollectionView()
+    }
+    
+    func updatePageControllContainerView() {
+        pageControl.numberOfPages = cellsIds.count
+        pageControl.currentPage = currentPageIndex
+    }
+    
+    var handleCollectionViewMovements = true
+    func updateCollectionView() {
+        handleCollectionViewMovements = false
+        let contentOffset = CGPoint(x: collectionView.frame.size.width * CGFloat(currentPageIndex), y: 0)
+        collectionView.setContentOffset(contentOffset, animated: true)
+    }
+    
+    @IBAction func skipTap(_ sender: Any) {
+        //present authentication screen
+    }
+    
+    @IBAction func nextTap(_ sender: Any) {
+        let newPageInde = currentPageIndex + 1
+        
+        if newPageInde < cellsIds.count {
+            currentPageIndex = newPageInde
+            return
+        }
+        
+        //present authentication screen
+    }
+}
+
+extension HowToViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        cellsIds.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        collectionView.dequeueReusableCell(withReuseIdentifier: cellsIds[indexPath.item], for: indexPath)
+    }
+    
+}
+
+extension HowToViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        collectionView.frame.size
+    }
+}
+
+extension HowToViewController: UICollectionViewDelegate {
+    
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        updateCurrentPage()
+        handleCollectionViewMovements = true
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didEndDisplaying cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        updateCurrentPage()
+        handleCollectionViewMovements = true
+    }
+    
+    public func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+        if !decelerate {
+            // cause by user
+            updateCurrentPage()
+            handleCollectionViewMovements = true
+        }
+    }
+    
+    public func scrollViewDidEndScrollingAnimation(_ scrollView: UIScrollView) {
+        //caused by code
+        updateCurrentPage()
+        handleCollectionViewMovements = true
+    }
+    
+    func updateCurrentPage() {
+        guard handleCollectionViewMovements else {
+            return
+        }
+        
+        guard let visibleCell = collectionView.visibleCells.first,
+              let indexPath = collectionView.indexPath(for: visibleCell) else {
+            return
+        }
+        
+        currentPageIndex = indexPath.row
+    }
 }
