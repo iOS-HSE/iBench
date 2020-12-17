7c49407278f64c1657aca225cdf643ba55bc23c7
ignatovskiy <naignatovskiy@edu.hse.ru>
2020-12-17 22:13:27 +0300

    WIP

---
+++ iBench/UI/Modules/UserProfileViewController/UserProfileViewController.swift
@@ -0,0 +1,9 @@
+//
+//  UserProfileViewController.swift
+//  iBench
+//
+//  Created by Hacker Man on 17.12.2020.
+//  Copyright © 2020 Андрей Журавлев. All rights reserved.
+//
+
+import Foundation
---
+++ iBench/UI/Modules/UserProfileViewController/UserProfileViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  UserProfileViewModel.swift
+//  iBench
+//
+//  Created by Hacker Man on 17.12.2020.
+//  Copyright © 2020 Андрей Журавлев. All rights reserved.
+//
+
+import Foundation
